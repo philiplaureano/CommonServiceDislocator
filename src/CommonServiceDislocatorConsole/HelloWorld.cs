2c347f81b96ed60e9ae6b7f16ef5f3fcc5d2ecb4
Philip Laureano <philip.laureano@gmail.com>
2010-05-02 10:27:07 +0800

    Initial commit

---
+++ src/CommonServiceDislocatorConsole/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CommonServiceDislocator.CommonServiceDislocatorConsole
+{
+	class HelloWorld
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World");
+		}
+	}
+}
+
